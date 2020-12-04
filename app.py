aed0809fffd89830d1c7065fc71452c5f318dbc9
Reshu1123 <46889422+Reshu1123@users.noreply.github.com>
2020-12-04 10:55:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # a route where we will display a welcome message via an HTML template
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('iindex.html')
     
 # run the application
 if __name__ == "__main__":
